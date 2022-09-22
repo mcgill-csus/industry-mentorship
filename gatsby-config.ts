ef253b4632838fed61f605907b1938376c8483fe
Sarvasv Arora <sarvasvarora@gmail.com>
2022-09-22 13:10:03 -0400

    gtag config fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -33,14 +33,13 @@ const config: GatsbyConfig = {
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
